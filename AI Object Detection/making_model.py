d5ce244006aad59d9e4a2a52dea0ebeb67fe323b
WhoopsoutSpy <connor.wadlin@gmail.com>
2026-02-11 13:23:38 -0500

    Delete AI Object Detection directory

--- AI Object Detection/making_model.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.yaml")
-
-results = model.train(data="C:/Users/conno/Python Files/Python Project/Work/Drone Competiton/Object Tracking/YOLO Files/circle_detection.yaml", epochs=50)
